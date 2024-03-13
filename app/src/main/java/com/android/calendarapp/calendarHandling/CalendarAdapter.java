a3342e87112651c63e7dccdc757b19dc5477b43d
Krataman <75064241+Krataman@users.noreply.github.com>
2024-03-13 16:42:53 +0100

    V 1.1: - created calendar_cell.xml - created CalendarViewHolder.java - created CalendarAdapter.java - implementation of required methods

---
+++ app/src/main/java/com/android/calendarapp/calendarHandling/CalendarAdapter.java
@@ -0,0 +1,24 @@
+package com.android.calendarapp.calendarHandling;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CalendarAdapter  extends RecyclerView.Adapter<CalendarViewHolder>{
+    @NonNull
+    @Override
+    public CalendarViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CalendarViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/android/calendarapp/calendarHandling/CalendarViewHolder.java
@@ -0,0 +1,12 @@
+package com.android.calendarapp.calendarHandling;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CalendarViewHolder extends RecyclerView.ViewHolder{
+    public CalendarViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
