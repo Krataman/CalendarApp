07eaa8e5b2d12b4f5cbd8936b920e0c2196dd615
Krataman <75064241+Krataman@users.noreply.github.com>
2024-03-13 17:40:22 +0100

    V 1.3: - created CalendarViewHolder.java

--- app/src/main/java/com/android/calendarapp/calendarHandling/CalendarViewHolder.java
+++ app/src/main/java/com/android/calendarapp/calendarHandling/CalendarViewHolder.java
@@ -1,12 +1,27 @@
 package com.android.calendarapp.calendarHandling;
 
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class CalendarViewHolder extends RecyclerView.ViewHolder{
-    public CalendarViewHolder(@NonNull View itemView) {
+import com.android.calendarapp.R;
+
+public class CalendarViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+    public final TextView dayOfMonth;
+    private final OnItemListener onItemListener;
+    public CalendarViewHolder(@NonNull View itemView, OnItemListener onItemListener) {
         super(itemView);
+        dayOfMonth = itemView.findViewById(R.id.cellDayText);
+        this.onItemListener = onItemListener;
+
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        onItemListener.onItemClick(getAdapterPosition(), (String) dayOfMonth.getText());
     }
 }
